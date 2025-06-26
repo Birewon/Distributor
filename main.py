6a946b3d4d07bfb3f217d6abacac69aaec2e1187
Birewon <pranikp52@gmail.com>
2025-06-27 01:22:43 +0300

    Add init_folders function

--- main.py
+++ main.py
@@ -5,4 +5,17 @@ This program sorts any files from the Downloads directory into any directory.
 Using libraries: os
 '''
 
+import os, shutil
+
+def init_folders():
+    download_path = r'C:\Users\PC\Downloads' # Your path
+    folders = ['Photo', 'Video', 'EXE', 'Files', 'Archives', 'Another'] # Folders
+    for folder in folders:
+        try:
+            os.mkdir(f'{download_path}\\{folder}')
+        except:
+            pass
+
+
+
 
